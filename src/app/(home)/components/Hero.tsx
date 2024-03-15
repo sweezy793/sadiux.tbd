c981c601d12e52d1a4e2d923f9ef533e91c51c20
sweezy793 <sarthaks793@gmail.com>
2024-03-16 03:52:23 +0530

    Some styling for hero component

--- src/app/(home)/components/Hero.tsx
+++ src/app/(home)/components/Hero.tsx
@@ -4,33 +4,55 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>
-        Hello there{" "}
-        <Image
-          className="inline"
-          src="https://cdn3.emoji.gg/emojis/8794-hellotherekenobi.png"
-          width={32}
-          height={32}
-          alt="HelloThereKenobi"
-        />
-        <br />
-        <span>{"I'm Sarthak Sarangi"}</span>
-      </h1>
-      <p>
-        {`Your friendly neighborhood software developer working at Freshworks, entrenched in the world of JavaScript and more. 
-        Beyond the code, I'm just a regular guy with a passion for all things tech.`}
-        <br />
-        {`By day, I'm crafting elegant solutions, but by night, I'm Batman(smh).`}
-        <br />
-        {`So, if you're up for a chat about coding challenges, the newest tech on the market, 
-        debating the best video games, or simply swapping recommendations, I'm all ears! Let's connect and explore the wonders of tech. 🚀`}
-      </p>
-      <Link href={"mailto:sarthaks793@gmail.com"}>
-        <div>
-          <h1>Contact Me 📫</h1>
+    <div className="min-h-[60vh] flex flex-col-reverse gap-16 lg:flex-row lg:gap-0 items-center justify-between">
+      <div className="space-y-10 text-center lg:text-left">
+        <h1 className="text-4xl lg:text-7xl font-bold">
+          Hello there{" "}
+          <Image
+            className="inline"
+            src="https://cdn3.emoji.gg/emojis/8794-hellotherekenobi.png"
+            width={50}
+            height={50}
+            alt="HelloThereKenobi"
+          />
+          <br />
+          <span className="underline underlin-offset-8 decoration-red-400">
+            {"I'm Sarthak Sarangi"}
+          </span>
+        </h1>
+        <p className="md:w-[50rem] text-lg text-gray-300">
+          {`Your friendly neighborhood software developer working at Freshworks with a passion for all things tech.`}
+          <br />
+          {`By day, I'm crafting elegant solutions, but by night, I'm Batman(smh).`}
+          <br />
+          {`So, if you're up for a chat about coding, new tech, 
+        debating about video games, or simply swapping recommendations, I'm all ears! Let's connect and explore the wonders of tech. 🚀`}
+        </p>
+        <Link
+          href={"mailto:sarthaks793@gmail.com"}
+          className="inline-block group"
+        >
+          <div>
+            <h1 className="text-3xl font-bold group-hover:text-red-400 transition-all">
+              Contact Me 📫
+            </h1>
+            <div className="w-40 h-2 bg-red-400 rounded-full "></div>
+            <div className="w-40 h-2 bg-indigo-400 rounded-full translate-x-2"></div>
+          </div>
+        </Link>
+      </div>
+      <div>
+        <div className="w-72 h-72 space-y-3 -rotate-[20deg]">
+          <div className="flex gap-3 translate-x-8">
+            <div className="w-36 h-36 rounded-full bg-red-400"></div>
+            <div className="w-0 h-0 border-t-[65px] border-t-transparent border-l-[100px] border-l-indigo-400 border-b-[65px] border-b-transparent"></div>
+          </div>
+          <div className="flex gap-3 -translate-x-16">
+            <div className="w-0 h-0 border-t-[65px] border-t-transparent border-r-[100px] border-r-indigo-400 border-b-[65px] border-b-transparent"></div>
+            <div className="w-36 h-36 rounded-full bg-red-400"></div>
+          </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 const page = () => {
   return (
     <div className="min-h-screen bg-black">
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto p-5  overflow-hidden">
         <Navbar />
         <Hero />
       </div>
